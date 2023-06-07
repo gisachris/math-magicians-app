c925b31e94e9cce84533a7f006bd6543e7976211
gisa chris <gisachris24@gmail.com>
2023-06-07 10:49:40 -0200

    fix linting errors

--- src/components/sideActions.js
+++ src/components/sideActions.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const SideBar = (props) => (
+const SideBar = ({
+  divide, multiply, subtract, add, equals,
+}) => (
   <section className="SideBar">
     <ul className="actionsList">
-      <li className="actionTask divide">{props.divide}</li>
-      <li className="actionTask multiply">{props.multiply}</li>
-      <li className="actionTask subtract">{props.subtract}</li>
-      <li className="actionTask add">{props.add}</li>
-      <li className="actionTask equals">{props.equals}</li>
+      <li className="actionTask divide">{divide}</li>
+      <li className="actionTask multiply">{multiply}</li>
+      <li className="actionTask subtract">{subtract}</li>
+      <li className="actionTask add">{add}</li>
+      <li className="actionTask equals">{equals}</li>
     </ul>
   </section>
 );
+
+SideBar.propTypes = {
+  divide: PropTypes.string,
+  multiply: PropTypes.string,
+  subtract: PropTypes.string,
+  add: PropTypes.string,
+  equals: PropTypes.string,
+};
 
 SideBar.defaultProps = {
   divide: '\u00F7',
--- src/components/topSection.js
+++ src/components/topSection.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TopSection = (props) => (
+const TopSection = ({ initial }) => (
   <section className="answer">
-    <input type="text" className="input" id="input" placeholder={props.initial} />
+    <input type="text" className="input" id="input" placeholder={initial} />
   </section>
 );
+
+TopSection.propTypes = {
+  initial: PropTypes.string,
+};
+
+TopSection.defaultProps = {
+  initial: 0,
+};
 
 export default TopSection;
