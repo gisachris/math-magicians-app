c4ed174869fef331111f9ff4c8c1448229336e6c
gisa chris <gisachris24@gmail.com>
2023-06-09 10:45:17 -0200

    fix linting errors

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,61 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState, useEffect, useCallback, useMemo,
+} from 'react';
 
 const Quote = () => {
-  const [data,setdata] = useState(null);
-  const [Isloading,setIsloading] = useState(false);
-  const [ErrorFound,setErrorFound] = useState(false);
+  const [data, setdata] = useState(null);
+  const [Isloading, setIsloading] = useState(false);
+  const [ErrorFound, setErrorFound] = useState(false);
   const baseUrl = 'https://api.api-ninjas.com/v1/quotes?category=';
   const baseKey = 'rBDqy7ELDjojxG7nokSQNQ==9rgsnCov5opWqsS6';
   const category = 'intelligence';
-  const fullUrl = baseUrl + category;
-  const init = {
-    method:'GET',
+  const fullUrl = useMemo(() => baseUrl + category, [baseUrl, category]);
+  const init = useMemo(() => ({
+    method: 'GET',
     headers: {
-      'X-Api-Key': baseKey
+      'X-Api-Key': baseKey,
     },
-    contentType : 'application/json'
-  }
+    contentType: 'application/json',
+  }), [baseKey]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      setIsloading(true);
+      const request = await fetch(fullUrl, init);
+      const response = await request.json();
+      setdata(response);
+      setIsloading(false);
+    } catch (error) {
+      setErrorFound(error.message);
+    }
+  }, [fullUrl, init]);
 
   useEffect(() => {
-    const fetchData = async() => {
-      try{
-        setIsloading(true)
-        const request = await fetch(fullUrl,init);
-        const response  = await request.json();
-        setdata(response);
-        setIsloading(false)
-      }catch(error){
-        setErrorFound(error.message)
-      }
-    }
     fetchData();
-    return(() => {
-      setdata(null)
-    })
-  },[]);
+    return () => {
+      setdata(null);
+    };
+  }, [fetchData]);
 
-  return (
-      Isloading ? (
-        <span className='loading generic'>qoute is loading...</span>
-      ):
-      ErrorFound ? (
-        <span className='error generic'><b>error found: </b> <i>{ErrorFound.message}
-  </i></span>
-      ):
-      (data === null) ? (
-        <span className='noQuo generic'>quote not found</span>
-      ):
-      data.map((item) => (
-        <section className='quoteSection'>
-          <span className='acQoute'>
-            {item.quote}
-            </span>
-          <h2 className='author'>By 
+  let content = '';
+  const stableId = 1;
+
+  if (Isloading) {
+    content = <span className="loading generic">quote is loading...</span>;
+  } else if (ErrorFound) {
+    content = (
+      <span className="error generic">
+        <b>error found: </b>
+        <i>{ErrorFound.message}</i>
+      </span>
+    );
+  } else if (data === null) {
+    content = <span className="noQuo generic">quote not found</span>;
+  } else {
+    content = data.map((item) => (
+      <section className="quoteSection" key={stableId}>
+        <span className="acQoute">{item.quote}</span>
+        <h2 className="author">
+          By
           {item.author}
-          </h2>
-        </section>
-      )))
-      
+        </h2>
+      </section>
+    ));
+  }
+
+  return content;
 };
 
 export default Quote;
